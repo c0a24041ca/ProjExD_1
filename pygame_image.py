c6d700b47881cffcc693f29d970714df3ac9994c
c0a24041 <c0a24041ca@edu.teu.ac.jp>
2025-11-18 16:31:00 +0900

    練習９残像問題

--- pygame_image.py
+++ pygame_image.py
@@ -10,17 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False) #練習8 背景画像左右反転
     kk_img = pg.image.load("fig/3.png") #練習3:こうかとん画像の読み込み
     kk_img = pg.transform.flip(kk_img, True, False) #練習3反転
     tmr = 0
-    x = 0
+    x = tmr %3200#練習９残像問題
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(bg_img, [-x+800, 0]) #練習7 こうかとんもう一匹
+        screen.blit(bg_img2, [-x+800, 0]) #練習7 こうかとんもう一匹
         screen.blit(bg_img, [-x, 0])#練習5背景移動
         screen.blit(kk_img,(300,200)) #練習4
         pg.display.update()
